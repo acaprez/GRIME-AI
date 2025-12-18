e8a81ea275a0629b76b311ad34a9288573ad13e7
Adam Caprez <acaprez2@unl.edu>
2025-12-18 23:47:25 +0000

    Add download script for SAM2 checkpoint files.

---
+++ recipe/post-link.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cat <<EOF >> ${PREFIX}/.messages.txt
+
+*************************************************************************************
+* NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE:                                *
+* Checkpoint files for SAM2 must be downloaded before running GRIME-AI.             *
+* Please run the included 'download-sam2-checkpoints.sh' script to fetch the files. *
+*************************************************************************************
+
+EOF
