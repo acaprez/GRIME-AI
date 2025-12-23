9ea86743a2802e3313d0d079a00af26ee2188e37
Adam Caprez <acaprez2@unl.edu>
2025-12-23 04:26:48 +0000

    Recipe fixes.

--- recipe/grime-ai-post/post-link.bat
+++ recipe/grime-ai-post/post-link.bat
@@ -1,12 +1,12 @@
 @echo off
 
 (
+echo
 echo **************************************************************************************
 echo * NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE:                                 *
 echo * Checkpoint files for SAM2 *must* be downloaded before running GRIME-AI.            *
-echo * Please run the command 'download-sam2-checkpoints' prior to using GRIME-AI.     *
+echo * Please run the command 'download-sam2-checkpoints' prior to using GRIME-AI.        *
 echo * This command will fetch the checkpoint files and save them to the proper location. *
 echo **************************************************************************************
+echo
 ) > %PREFIX%\.messages.txt
-
-
