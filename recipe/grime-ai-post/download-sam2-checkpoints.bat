8f1e8d561c86113d75656e5f6407c5edda2cf6a2
Adam Caprez <acaprez2@unl.edu>
2026-01-06 20:36:37 +0000

    Update post recipe.

--- recipe/grime-ai-post/download-sam2-checkpoints.bat
+++ recipe/grime-ai-post/download-sam2-checkpoints.bat
@@ -1,5 +1,3 @@
 @echo off
 
-"/opt/anaconda1anaconda2anaconda3"\python.exe "/opt/anaconda1anaconda2anaconda3"\Scripts\download-sam2-checkpoints.py
-
-echo "/opt/anaconda1anaconda2anaconda3"
+"/opt/anaconda1anaconda2anaconda3"\python.exe "/opt/anaconda1anaconda2anaconda3"\download-sam2-checkpoints.py
