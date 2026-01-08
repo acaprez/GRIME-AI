aead0950587d3d517af9100c0fd7b8fc9a963708
Adam Caprez <acaprez2@unl.edu>
2026-01-08 00:16:57 +0000

    Clarify verbiage.

--- recipe/grime-ai-post/post-link.bat
+++ recipe/grime-ai-post/post-link.bat
@@ -5,8 +5,9 @@ echo
 echo **************************************************************************************
 echo * NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE:                                 *
 echo * Checkpoint files for SAM2 *must* be downloaded before running GRIME-AI.            *
-echo * Please run the command 'download-sam2-checkpoints' prior to using GRIME-AI.        *
-echo * This command will fetch the checkpoint files and save them to the proper location. *
+echo * Please activate the environment and run the command 'download-sam2-checkpoints'    *
+echo * prior to using GRIME-AI.  This command will fetch the checkpoint files and save    *
+echo * them to the proper location.                                                       *
 echo **************************************************************************************
 echo
 ) > %PREFIX%\.messages.txt
--- recipe/grime-ai-post/post-link.sh
+++ recipe/grime-ai-post/post-link.sh
@@ -5,8 +5,9 @@ cat <<EOF >> ${PREFIX}/.messages.txt
 **************************************************************************************
 * NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE:                                 *
 * Checkpoint files for SAM2 *must* be downloaded before running GRIME-AI.            *
-* Please run the command 'download-sam2-checkpoints' prior to using GRIME-AI.        *
-* This command will fetch the checkpoint files and save them to the proper location. *
+* Please activate the environment and run the command 'download-sam2-checkpoints'    *
+* prior to using GRIME-AI.  This command will fetch the checkpoint files and save    *
+* them to the proper location.                                                       *
 **************************************************************************************
 
 EOF
